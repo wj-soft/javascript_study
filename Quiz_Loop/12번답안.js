dc3bc13d01e3eb74dbb7ba480158cb7a9208bbba
wj-soft <wooju_lee@daum.net>
2017-09-29 15:41:04 +0900

    add function

--- Quiz_Loop/12번답안.js
+++ Quiz_Loop/12번답안.js
@@ -1,11 +1,27 @@
-for (i = 0; i < 5; i++) {
-  var result = "";
-  for (j = i; j < 4; j++) {
-    result += ' ';
-  }
-  for (k = 0; k < i * 2 + 1; k++) {
+// for (i = 0; i < 5; i++) {
+//   var result = "";
+//   for (j = i; j < 4; j++) {
+//     result += ' ';
+//   }
+//   for (k = 0; k < i * 2 + 1; k++) {
 
-    result += '*';
+//     result += '*';
+//   }
+//   console.log(result);
+// }
+
+
+function makeTriangle(x) {
+  for (i = 0; i < x; i++) {
+    var result = "";
+    for (j = i; j < x - 1; j++) {
+      result += ' ';
+    }
+    for (k = 0; k < i * 2 + 1; k++) {
+
+      result += '*';
+    }
+    console.log(result);
   }
-  console.log(result);
 }
+a = makeTriangle(10);
--- Quiz_Loop/13번답안.js
+++ Quiz_Loop/13번답안.js
@@ -1,10 +1,24 @@
-for (i = 0; i < 5; i++) {
-  var result = '';
-  for (j = 0; j < i; j++) {
-    result += ' ';
+// for (i = 0; i < 5; i++) {
+//   var result = '';
+//   for (j = 0; j < i; j++) {
+//     result += ' ';
+//   }
+//   for (k = 9; k > i * 2; k--) {
+//     result += '*';
+//   }
+//   console.log(result)
+// }
+
+var makeTriangle = function(x) {
+  for (i = 0; i < x; i++) {
+    var result = '';
+    for (j = 0; j < i; j++) {
+      result += ' ';
+    }
+    for (k = x * 2 - 1; k > i * 2; k--) {
+      result += '*';
+    }
+    console.log(result)
   }
-  for (k = 9; k > i * 2; k--) {
-    result += '*';
-  }
-  console.log(result)
 }
+makeTriangle(1)
